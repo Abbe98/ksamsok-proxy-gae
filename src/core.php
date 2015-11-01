d12924b1498416db6d7188984fb2268d8244cbda
Albin Larsson <albin.post@gmail.com>
2015-11-01 22:19:21 +0100

    CORS! :ice_cream:

--- src/core.php
+++ src/core.php
@@ -10,5 +10,11 @@ function sochQueryRequest($queryString, $sochEndpoint) {
     header($header);
   }
 
+  if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');
+  }
+
   echo $content;
 }
