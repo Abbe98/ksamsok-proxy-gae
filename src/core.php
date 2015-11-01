c222f11750317ebc50c07558ec62758989b41403
Albin Larsson <albin.post@gmail.com>
2015-11-01 22:36:42 +0100

    Minor CORS fix
    GAE did not seam to support HTTP_ORIGIN

--- src/core.php
+++ src/core.php
@@ -10,11 +10,8 @@ function sochQueryRequest($queryString, $sochEndpoint) {
     header($header);
   }
 
-  if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');
-  }
+  header('Access-Control-Allow-Origin: *');
+
 
   echo $content;
 }
