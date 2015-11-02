907a40390f0d1378635e91a3989d17aade8e4e6d
Albin Larsson <albin.post@gmail.com>
2015-11-02 11:14:19 +0100

    Support getting JSON-LD from http headers

--- src/core.php
+++ src/core.php
@@ -3,7 +3,26 @@ $sochEndpoint = 'http://kulturarvsdata.se/';
 
 function sochQueryRequest($queryString, $sochEndpoint) {
   $url = $sochEndpoint . $queryString;
-  $content = file_get_contents($url);
+
+  $incomingHeaders = getallheaders();
+  if (array_key_exists('Accept format', $incomingHeaders) && ($incomingHeaders == 'application/json' || 'application/json-ld' || 'application/ld-json')) {
+    $opts = array(
+      'http' => array(
+        'method' => "GET",
+        'header' => "Accept format: application/json\r\n"
+      )
+    );
+  } else {
+    $opts = array(
+      'http' => array(
+        'method' => "GET",
+        'header' => "Accept format: *\r\n"
+      )
+    );
+  }
+
+  $context = stream_context_create($opts);
+  $content = file_get_contents($url, false, $context);
 
   foreach ($http_response_header as $header) {
     header($header);
